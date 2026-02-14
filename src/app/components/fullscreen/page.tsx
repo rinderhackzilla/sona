698e954b4cf8373d0fdda5810cf6658f8a1cf3c2
Joe <j.uessem@gmail.com>
2026-02-14 18:47:41 +0100

    Wrap fullscreen mode with visualizer provider

--- src/app/components/fullscreen/page.tsx
+++ src/app/components/fullscreen/page.tsx
@@ -6,6 +6,7 @@ import {
   DrawerTrigger,
 } from '@/app/components/ui/drawer'
 import { useAppWindow } from '@/app/hooks/use-app-window'
+import { VisualizerProvider } from './settings'
 import { FullscreenBackdrop } from './backdrop'
 import { FullscreenDragHandler } from './drag-handler'
 import { FullscreenPlayer } from './player'
@@ -21,39 +22,41 @@ export function FullscreenMode({ children }: FullscreenModeProps) {
   const { handleFullscreen } = useAppWindow()
 
   return (
-    <Drawer
-      onAnimationEnd={handleFullscreen}
-      fixed={true}
-      handleOnly={true}
-      disablePreventScroll={true}
-      dismissible={true}
-      modal={false}
-    >
-      <DrawerTrigger asChild>{children}</DrawerTrigger>
-      <DrawerTitle className="sr-only">Big Player</DrawerTitle>
-      <DrawerContent
-        className="h-screen w-screen rounded-t-none border-none select-none cursor-default mt-0"
-        showHandle={false}
-        aria-describedby={undefined}
+    <VisualizerProvider>
+      <Drawer
+        onAnimationEnd={handleFullscreen}
+        fixed={true}
+        handleOnly={true}
+        disablePreventScroll={true}
+        dismissible={true}
+        modal={false}
       >
-        <MemoFullscreenBackdrop />
-        <FullscreenDragHandler />
-        <div className="absolute inset-0 flex flex-col p-0 2xl:p-8 pt-10 2xl:pt-12 w-full h-full gap-4 bg-black/0 z-10">
-          {/* First Row */}
-          <div className="w-full max-h-[calc(100%-180px)] min-h-[calc(100%-180px)] px-8 2xl:px-16 pt-4 2xl:pt-8">
-            <div className="min-h-[300px] h-full max-h-full">
-              <FullscreenTabs />
+        <DrawerTrigger asChild>{children}</DrawerTrigger>
+        <DrawerTitle className="sr-only">Big Player</DrawerTitle>
+        <DrawerContent
+          className="h-screen w-screen rounded-t-none border-none select-none cursor-default mt-0"
+          showHandle={false}
+          aria-describedby={undefined}
+        >
+          <MemoFullscreenBackdrop />
+          <FullscreenDragHandler />
+          <div className="absolute inset-0 flex flex-col p-0 2xl:p-8 pt-10 2xl:pt-12 w-full h-full gap-4 bg-black/0 z-10">
+            {/* First Row */}
+            <div className="w-full max-h-[calc(100%-180px)] min-h-[calc(100%-180px)] px-8 2xl:px-16 pt-4 2xl:pt-8">
+              <div className="min-h-[300px] h-full max-h-full">
+                <FullscreenTabs />
+              </div>
             </div>
-          </div>
 
-          {/* Second Row */}
-          <div className="h-[150px] min-h-[150px] px-8 2xl:px-16 py-2">
-            <div className="flex items-center">
-              <FullscreenPlayer />
+            {/* Second Row */}
+            <div className="h-[150px] min-h-[150px] px-8 2xl:px-16 py-2">
+              <div className="flex items-center">
+                <FullscreenPlayer />
+              </div>
             </div>
           </div>
-        </div>
-      </DrawerContent>
-    </Drawer>
+        </DrawerContent>
+      </Drawer>
+    </VisualizerProvider>
   )
 }
