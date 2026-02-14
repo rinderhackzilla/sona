6c17cad777c45fba6dc1fa0a3d517fce4aea74fb
Joe <j.uessem@gmail.com>
2026-02-14 17:57:54 +0100

    Use correct Material Design Equalizer icon from uploaded image

--- src/app/components/icons/equalizer.tsx
+++ src/app/components/icons/equalizer.tsx
@@ -2,11 +2,11 @@ export function EqualizerIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 -960 960 960"
       fill="currentColor"
       className={className}
     >
-      <path d="M10 20h4V4h-4v16zm-6 0h4v-8H4v8zM16 9v11h4V9h-4z" />
+      <path d="M280-280v-400h80v400h-80Zm160 120v-640h80v640h-80Zm160-240v-160h80v160h-80Z" />
     </svg>
   )
 }
