c797ab40ac8c9160b2c65111e048685ec73782ca
Joe <j.uessem@gmail.com>
2026-02-14 18:07:49 +0100

    Create correct 5-line symmetric equalizer icon

--- src/app/components/icons/equalizer.tsx
+++ src/app/components/icons/equalizer.tsx
@@ -2,13 +2,15 @@ export function EqualizerIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="24"
-      viewBox="0 -960 960 960"
-      width="24"
+      viewBox="0 0 24 24"
       fill="currentColor"
       className={className}
     >
-      <path d="M280-280v-400h80v400h-80Zm160 120v-640h80v640h-80Zm160-240v-160h80v160h-80Z" />
+      <rect x="2" y="8" width="2" height="8" rx="1" />
+      <rect x="6" y="4" width="2" height="16" rx="1" />
+      <rect x="10" y="2" width="2" height="20" rx="1" />
+      <rect x="14" y="4" width="2" height="16" rx="1" />
+      <rect x="18" y="8" width="2" height="8" rx="1" />
     </svg>
   )
 }
