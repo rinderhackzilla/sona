ad26c5f963e093eabda938052005d7dd9c17cb01
Joe <j.uessem@gmail.com>
2026-02-15 09:26:54 +0100

    Remove pulsing orb from visualizer list

--- src/app/components/fullscreen/visualizers/index.ts
+++ src/app/components/fullscreen/visualizers/index.ts
@@ -3,11 +3,9 @@ import { FrequencyCircle } from './frequency-circle'
 import { GeometricMandala } from './geometric-mandala'
 import { ParticleNebula } from './particle-nebula'
 import { ParticleRing } from './particle-ring'
-import { PulsingOrb } from './pulsing-orb'
 import { RadialSpectrum } from './radial-spectrum'
 
 export const VISUALIZERS = {
-  'pulsing-orb': PulsingOrb,
   'particle-ring': ParticleRing,
   'radial-spectrum': RadialSpectrum,
   'frequency-circle': FrequencyCircle,
@@ -19,7 +17,6 @@ export const VISUALIZERS = {
 export type VisualizerPreset = keyof typeof VISUALIZERS
 
 export const VISUALIZER_NAMES: Record<VisualizerPreset, string> = {
-  'pulsing-orb': 'Pulsing Orb',
   'particle-ring': 'Particle Ring',
   'radial-spectrum': 'Radial Spectrum',
   'frequency-circle': 'Frequency Circle',
