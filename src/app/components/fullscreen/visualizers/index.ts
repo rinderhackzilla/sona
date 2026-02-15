dd306d4b540de67586e44f29cf981a275ad1557d
Joe <j.uessem@gmail.com>
2026-02-15 01:03:44 +0100

    Add missing visualizers and remove wave-circle

--- src/app/components/fullscreen/visualizers/index.ts
+++ src/app/components/fullscreen/visualizers/index.ts
@@ -1,23 +1,41 @@
+import { AudioLandscape } from './audio-landscape'
+import { Butterchurn } from './butterchurn'
+import { CircularWaveform } from './circular-waveform'
 import { FrequencyCircle } from './frequency-circle'
+import { GeometricMandala } from './geometric-mandala'
+import { Oscilloscope } from './oscilloscope'
+import { ParticleNebula } from './particle-nebula'
+import { ParticleRing } from './particle-ring'
 import { PulsingOrb } from './pulsing-orb'
 import { RadialSpectrum } from './radial-spectrum'
-import { ParticleRing } from './particle-ring'
-import { WaveCircle } from './wave-circle'
+import { WaveformTunnel } from './waveform-tunnel'
 
 export const VISUALIZERS = {
-  'frequency-circle': FrequencyCircle,
   'pulsing-orb': PulsingOrb,
-  'radial-spectrum': RadialSpectrum,
   'particle-ring': ParticleRing,
-  'wave-circle': WaveCircle,
+  'radial-spectrum': RadialSpectrum,
+  'frequency-circle': FrequencyCircle,
+  'geometric-mandala': GeometricMandala,
+  'circular-waveform': CircularWaveform,
+  'particle-nebula': ParticleNebula,
+  'audio-landscape': AudioLandscape,
+  'waveform-tunnel': WaveformTunnel,
+  'oscilloscope': Oscilloscope,
+  'butterchurn': Butterchurn,
 } as const
 
 export type VisualizerPreset = keyof typeof VISUALIZERS
 
 export const VISUALIZER_NAMES: Record<VisualizerPreset, string> = {
-  'frequency-circle': 'Frequency Circle',
   'pulsing-orb': 'Pulsing Orb',
-  'radial-spectrum': 'Radial Spectrum',
   'particle-ring': 'Particle Ring',
-  'wave-circle': 'Wave Circle',
+  'radial-spectrum': 'Radial Spectrum',
+  'frequency-circle': 'Frequency Circle',
+  'geometric-mandala': 'Geometric Mandala',
+  'circular-waveform': 'Circular Waveform',
+  'particle-nebula': 'Particle Nebula',
+  'audio-landscape': 'Audio Landscape',
+  'waveform-tunnel': 'Waveform Tunnel',
+  'oscilloscope': 'Oscilloscope',
+  'butterchurn': 'Butterchurn',
 }
