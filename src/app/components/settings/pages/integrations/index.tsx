b624b775026f475a8159b84fc9c55b879100db88
Joe <j.uessem@gmail.com>
2026-02-15 10:37:13 +0100

    Add Last.fm to integrations settings page

--- src/app/components/settings/pages/integrations/index.tsx
+++ src/app/components/settings/pages/integrations/index.tsx
@@ -1,8 +1,10 @@
+import { LastFM } from './lastfm'
 import { Lidarr } from './lidarr'
 
 export function Integrations() {
   return (
     <div className="space-y-4">
+      <LastFM />
       <Lidarr />
     </div>
   )
