c6a889c725eac626a1a90118c9e18bbc0ccd8712
Joe <j.uessem@gmail.com>
2026-02-15 21:59:19 +0100

    Add Rabbit Hole option to song context menu

--- src/app/components/song/menu-options.tsx
+++ src/app/components/song/menu-options.tsx
@@ -1,7 +1,14 @@
+import { Rabbit } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
 import { OptionsButtons } from '@/app/components/options/buttons'
 import { DownloadOptionHandler } from '@/app/components/options/download-handler'
-import { ContextMenuSeparator } from '@/app/components/ui/context-menu'
+import {
+  ContextMenuItem,
+  ContextMenuSeparator,
+} from '@/app/components/ui/context-menu'
+import { DropdownMenuItem } from '@/app/components/ui/dropdown-menu'
 import { useOptions } from '@/app/hooks/use-options'
+import { useRabbitHole } from '@/app/hooks/use-rabbit-hole'
 import { ISong } from '@/types/responses/song'
 import { AddToPlaylistSubMenu } from './add-to-playlist'
 
@@ -16,6 +23,7 @@ export function SongMenuOptions({
   song,
   index,
 }: SongMenuOptionsProps) {
+  const { t } = useTranslation()
   const {
     playNext,
     playLast,
@@ -26,7 +34,20 @@ export function SongMenuOptions({
     openSongInfo,
     isOnPlaylistPage,
   } = useOptions()
+  const { startRabbitHole, isLoading } = useRabbitHole()
   const songIndexes = [index.toString()]
+
+  const handleRabbitHole = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    startRabbitHole({
+      type: 'artist',
+      artistName: song.artist,
+      artistId: song.artistId,
+      trackName: song.title,
+    })
+  }
+
+  const RabbitHoleMenuItem = variant === 'context' ? ContextMenuItem : DropdownMenuItem
 
   return (
     <>
@@ -44,6 +65,15 @@ export function SongMenuOptions({
           playLast([song])
         }}
       />
+      <ContextMenuSeparator />
+      <RabbitHoleMenuItem
+        onClick={handleRabbitHole}
+        disabled={isLoading}
+        className="cursor-pointer"
+      >
+        <Rabbit className="mr-2 h-4 w-4" />
+        <span>{t('rabbitHole.button')}</span>
+      </RabbitHoleMenuItem>
       <ContextMenuSeparator />
       <OptionsButtons.AddToPlaylistOption variant={variant}>
         <AddToPlaylistSubMenu
