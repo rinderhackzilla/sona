f29108680ce2726fb81319235e56b5ea546b6a2c
Joe <j.uessem@gmail.com>
2026-02-15 22:15:57 +0100

    Add Rabbit Hole to album context menu

--- src/app/components/album/options.tsx
+++ src/app/components/album/options.tsx
@@ -1,8 +1,12 @@
+import { Rabbit } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
+import { useRabbitHole } from '@/app/hooks/use-rabbit-hole'
 import { OptionsButtons } from '@/app/components/options/buttons'
 import { DownloadOptionHandler } from '@/app/components/options/download-handler'
 import { AddToPlaylistSubMenu } from '@/app/components/song/add-to-playlist'
 import {
   DropdownMenuGroup,
+  DropdownMenuItem,
   DropdownMenuSeparator,
 } from '@/app/components/ui/dropdown-menu'
 import { useOptions } from '@/app/hooks/use-options'
@@ -13,6 +17,7 @@ interface AlbumOptionsProps {
 }
 
 export function AlbumOptions({ album }: AlbumOptionsProps) {
+  const { t } = useTranslation()
   const {
     playNext,
     playLast,
@@ -20,6 +25,7 @@ export function AlbumOptions({ album }: AlbumOptionsProps) {
     addToPlaylist,
     createNewPlaylist,
   } = useOptions()
+  const { startRabbitHole, isLoading: rabbitHoleLoading } = useRabbitHole()
 
   function handlePlayNext() {
     playNext(album.song)
@@ -45,11 +51,31 @@ export function AlbumOptions({ album }: AlbumOptionsProps) {
     createNewPlaylist(album.name, songIdToAdd)
   }
 
+  function handleRabbitHole() {
+    startRabbitHole({
+      type: 'album',
+      artistName: album.artist,
+      albumName: album.name,
+      albumId: album.id,
+    })
+  }
+
   return (
     <>
       <DropdownMenuGroup>
         <OptionsButtons.PlayNext onClick={handlePlayNext} />
         <OptionsButtons.PlayLast onClick={handlePlayLast} />
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem
+          onClick={handleRabbitHole}
+          disabled={rabbitHoleLoading}
+          className="gap-2"
+        >
+          <Rabbit className="h-4 w-4" />
+          <span>{t('rabbitHole.button')}</span>
+        </DropdownMenuItem>
       </DropdownMenuGroup>
       <DropdownMenuSeparator />
       <OptionsButtons.AddToPlaylistOption variant="dropdown">
