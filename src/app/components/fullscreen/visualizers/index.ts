f004329e19f5eb0944756f0645bd1b45cfc06de4
Joe <j.uessem@gmail.com>
2026-02-15 01:16:06 +0100

    Update visualizer index: remove deleted visualizers

--- src/app/components/fullscreen/visualizers/index.ts
+++ src/app/components/fullscreen/visualizers/index.ts
@@ -1,14 +1,10 @@
 import { AudioLandscape } from './audio-landscape'
-import { ButterchurnVisualizer } from './butterchurn'
-import { CircularWaveform } from './circular-waveform'
 import { FrequencyCircle } from './frequency-circle'
 import { GeometricMandala } from './geometric-mandala'
-import { Oscilloscope } from './oscilloscope'
 import { ParticleNebula } from './particle-nebula'
 import { ParticleRing } from './particle-ring'
 import { PulsingOrb } from './pulsing-orb'
 import { RadialSpectrum } from './radial-spectrum'
-import { WaveformTunnel } from './waveform-tunnel'
 
 export const VISUALIZERS = {
   'pulsing-orb': PulsingOrb,
@@ -16,12 +12,8 @@ export const VISUALIZERS = {
   'radial-spectrum': RadialSpectrum,
   'frequency-circle': FrequencyCircle,
   'geometric-mandala': GeometricMandala,
-  'circular-waveform': CircularWaveform,
   'particle-nebula': ParticleNebula,
   'audio-landscape': AudioLandscape,
-  'waveform-tunnel': WaveformTunnel,
-  'oscilloscope': Oscilloscope,
-  'butterchurn': ButterchurnVisualizer,
 } as const
 
 export type VisualizerPreset = keyof typeof VISUALIZERS
@@ -32,10 +24,6 @@ export const VISUALIZER_NAMES: Record<VisualizerPreset, string> = {
   'radial-spectrum': 'Radial Spectrum',
   'frequency-circle': 'Frequency Circle',
   'geometric-mandala': 'Geometric Mandala',
-  'circular-waveform': 'Circular Waveform',
   'particle-nebula': 'Particle Nebula',
   'audio-landscape': 'Audio Landscape',
-  'waveform-tunnel': 'Waveform Tunnel',
-  'oscilloscope': 'Oscilloscope',
-  'butterchurn': 'Butterchurn',
 }
