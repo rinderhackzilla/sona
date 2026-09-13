03e3eb65c90adc6f1cfed435a87326e2acf78b76
Joe <j.uessem@gmail.com>
2026-09-13 20:52:29 +0200

    chore: remove legacy docker, vercel, and pnpm artifacts

--- set-variables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Replace environment variables on the template and create the definitive config file.
-envsubst < /usr/share/nginx/html/env-config.js.template > /usr/share/nginx/html/env-config.js
